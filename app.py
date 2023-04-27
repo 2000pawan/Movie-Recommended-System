34ce2a8f0f1107e04964df080db1960bcf3d135f
PAWAN YADAV <129971371+2000pawan@users.noreply.github.com>
2023-04-28 00:20:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ import streamlit as st
 import pickle
 import pandas as pd
 import requests
+from PIL import Image
+
+image = Image.open('img.jpg')
+
+st.image(image, caption='Movie Recommended System')  
 
 def fetch_poster(movie_id):
     url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
